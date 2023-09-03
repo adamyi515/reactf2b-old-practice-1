a496874f60f679eb09e6e2fe0cd4816b6064072c
adamyi515 <adamyi515@gmail.com>
2023-09-03 14:49:01 -0500

    Install bootstrap and create components.

---
+++ src/pages/home-page.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <>
+        <h1 className="display-4 mb-2">
+          <span className="text-danger">Contact</span> List
+        </h1>
+        
+      </>
+  )
+}
+
+export default HomePage
