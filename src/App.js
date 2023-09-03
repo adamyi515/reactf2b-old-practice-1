3d48a6890b56d30194f9a2c671c74f12fb86d704
adamyi515 <adamyi515@gmail.com>
2023-09-03 14:48:34 -0500

    Setup react-router-dom and add header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Components
+import Header from './components/header/header.component';
+
+// Pages
+import HomePage from './pages/home-page.component';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <Router>
+        <div className="App">
+          <Header branding="Contact Manager" />
+          <div className="container">
+            <Routes>
+              <Route path='/' element={<HomePage />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
   );
 }
 
