4f97205a140458beabff7e7f41a53bf7fce26bdc
adamyi515 <adamyi515@gmail.com>
2023-09-03 15:19:37 -0500

    Create a form to add contact information on the AddContact page.'

---
+++ src/components/contacts/ContactList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactList = () => {
+  return (
+    <div className='contact-list'>
+        
+    </div>
+  )
+}
+
+export default ContactList
--- src/pages/AddContact.jsx
+++ src/pages/AddContact.jsx
@@ -1,14 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddContact = () => {
-  return (
-    <div className='add-contact'>
-        <h1>Add Contact</h1>
-        <form>
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        phone: ''
+    });
 
-        </form>
-    </div>
-  )
+    const { name, email, phone } = formData;
+
+    // Event Handlers //////////////////////////////////////////////////////////////////////////////////////////////////////
+    function handleSubmit(ev) {
+        ev.preventDefault();
+    }
+
+    function handleTextChange(ev) {
+        setFormData(prevState => {
+            return {
+                ...prevState,
+                [ev.target.name]: ev.target.value
+            }
+        });
+    }
+
+    return (
+        <div className='add-contact'>
+            <h1>Add Contact</h1>
+            <form onSubmit={handleSubmit}>
+                <div className='form-group'>
+                    <input type='text' className='form-control' value={name} name='name' 
+                        onChange={handleTextChange} placeholder='Enter name' />
+                </div>
+                <div className='form-group'>
+                    <input type='email' className='form-control' value={email} name='email'
+                        onChange={handleTextChange} placeholder='Enter email' />
+                </div>
+                <div className='form-group'>
+                    <input type='text' className='form-control' value={phone} name='phone'
+                        onChange={handleTextChange} placeholder='Enter phone' />
+                </div>
+                <div className='form-group'>
+                    <button className='btn btn-light form-control'>Add Contact</button>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default AddContact
