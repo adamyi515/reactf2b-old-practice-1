dd14028ab7ec9ba4a93442513d36728f446b492e
adamyi515 <adamyi515@gmail.com>
2023-09-03 17:55:20 -0500

    Create the structure of ContactList component and output the list of contacts from dummy data.

--- src/components/contacts/ContactList.jsx
+++ src/components/contacts/ContactList.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
+const dummyData = [
+    { "id": 1, "name": "Adam", "email": "Adam@test.com", "phone": "123-1234"},
+    { "id": 2, "name": "Steve", "email": "Steve@test.com", "phone": "123-1234"},
+    { "id": 3, "name": "David", "email": "David@test.com", "phone": "123-1234"}
+]
+
 const ContactList = () => {
   return (
     <div className='contact-list'>
-        
+        {dummyData.map(contact => {
+          return(
+            <div className='contact'>
+              <div className='contact-header'>
+                <h4>{ contact.name }</h4>
+                <div className='contact-buttons'>
+                  <button className='btn btn-light'>Edit</button>
+                  <button className='btn btn-light'>Delete</button>
+                </div>  
+              </div>
+              <div className='contact-body'>
+                <p>Email: { contact.email } </p>
+                <p>Phone: { contact.phone } </p>
+              </div>
+            </div>
+          )
+        })}
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import ContactList from '../components/contacts/ContactList'
 
 const Home = () => {
   return (
     <>
         <h1 className="display-4 mb-2">
           <span className="text-danger">Contact</span> List
+          <ContactList />
         </h1>
         
       </>
